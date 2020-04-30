9a201d73f6a677364eabd5ead2885f20a82740ec
Girard Vincent <girvincent@gmail.com>
2020-04-30 16:00:13 +0200

    mise à jour route vincent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PierreBatignesBioComponent } from './pierre-batignes-bio/pierre-batignes-bio.component'
+import { VincentGComponent } from './vincent-g/vincent-g.component';
 
 export const ROUTES: Routes = [
-    { path: 'pierre', component: PierreBatignesBioComponent }
+    { path: 'pierre', component: PierreBatignesBioComponent },
+    { path: 'vincent', component: VincentGComponent },
 ];
