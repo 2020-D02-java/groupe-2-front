904f1f513001356634e215f4f12a016c76fc63a7
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-04-30 15:18:06 +0200

    Creer page bio : composant anais #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,26 @@ import {StatutConnecteService} from './auth/statut-connecte.service';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { AnaisComponent } from './anais/anais.component';
 
+
+export const ROUTES: Routes = [
+  { path: 'anais', component: AnaisComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    TechComponent
+    TechComponent,
+    AnaisComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
