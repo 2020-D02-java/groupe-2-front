e3f07dfda8639e47f454671e01f671f88375e207
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-04-30 16:41:04 +0200

    jonathan Bio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { PierreBatignesBioComponent } from './pierre-batignes-bio/pierre-batignes-bio.component'
 import { VincentGComponent } from './vincent-g/vincent-g.component';
 import { AnaisComponent } from './anais/anais.component';
+import { JonathanBioComponent } from './jonathan-bio/jonathan-bio.component';
 export const ROUTES: Routes = [
     { path: 'pierre', component: PierreBatignesBioComponent },
 
     { path: 'vincent', component: VincentGComponent },
 
-    { path: 'anais', component: AnaisComponent}
+    { path: 'anais', component: AnaisComponent},
+
+    { path: 'jonathan', component: JonathanBioComponent},
+
 ];
---
+++ src/app/jonathan-bio/jonathan-bio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jonathan-bio',
+  templateUrl: './jonathan-bio.component.html',
+  styleUrls: ['./jonathan-bio.component.scss']
+})
+export class JonathanBioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
